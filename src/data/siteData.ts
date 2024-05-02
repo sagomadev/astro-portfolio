3038c133be1043d0b2512bb09dbaa4a3400dc3d4
Samuel Gómez <samuel.gomez@esave.es>
2024-05-02 13:06:59 +0200

    Add Layout and base components

---
+++ src/data/siteData.ts
@@ -0,0 +1,5 @@
+export const baseData = {
+  title: "Samuel Gómez Portfolio Site",
+  description:
+    "Bienvenido a mi portafolio personal, aquí podrás encontrar información sobre mi experiencia profesional, proyectos personales y mucho más.",
+};
